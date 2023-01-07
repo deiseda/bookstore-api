e6d5e0ef28de5511454a5f64713189740558b4b5
deiseda <deiseda@gmail.com>
2023-01-07 16:01:36 -0300

    finalizando as classes de dominio

--- src/main/java/com/deise/bookstore/domain/Livro.java
+++ src/main/java/com/deise/bookstore/domain/Livro.java
@@ -1,8 +1,19 @@
 package com.deise.bookstore.domain;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public class Livro {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Livro implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String titulo;
 	private String nome_autor;
