afaaaca7070e52c17e5b643d5e3d5fe49f7f4dcf
deiseda <deiseda@gmail.com>
2023-01-10 17:54:59 -0300

    POST e DELETE para livros

--- src/main/java/com/deise/bookstore/service/LivroService.java
+++ src/main/java/com/deise/bookstore/service/LivroService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.deise.bookstore.domain.Categoria;
 import com.deise.bookstore.domain.Livro;
 import com.deise.bookstore.repositories.LivroRepository;
 import com.deise.bookstore.service.exceptions.ObjectNotFoundException;
@@ -39,5 +40,17 @@ public class LivroService {
 		newObj.setTitulo(obj.getTitulo());	
 		newObj.setNome_autor(obj.getNome_autor());
 		newObj.setTexto(obj.getTexto());
+	}
+
+	public Livro create(Integer id_cat, Livro obj) {
+		obj.setId(null);
+		Categoria cat = categoriaService.findById(id_cat);
+		obj.setCategoria(cat);
+		return repository.save(obj);
+	}
+
+	public void delete(Integer id) {
+		Livro obj = findById(id);
+		repository.delete(obj);
 	}
 }
