890ff2550af468e574c2fe7ae0d07369411fe8b6
deiseda <deiseda@gmail.com>
2023-01-10 05:08:46 -0300

    Get para livro por ID

---
+++ src/main/java/com/deise/bookstore/service/LivroService.java
@@ -0,0 +1,22 @@
+package com.deise.bookstore.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.deise.bookstore.domain.Livro;
+import com.deise.bookstore.repositories.LivroRepository;
+import com.deise.bookstore.service.exceptions.ObjectNotFoundException;
+
+@Service
+public class LivroService {
+	
+	@Autowired
+	private LivroRepository repository;
+
+	public Livro findById(Integer id) {
+		Optional<Livro> obj = repository.findById(id);
+		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Livro.class.getName()));
+	}
+}
